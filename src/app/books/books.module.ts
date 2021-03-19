30b59dad195d80d0bca0f6f385cbc87416b9efbd
Michel Vollebregt <>
2021-03-19 17:38:32 +0100

    Added assignments and fixed setup

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,15 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {BookComponent} from './book/book.component';
-import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    BookComponent,
-    TesteComponent
+    BookComponent
   ],
   exports: [
     BookComponent
